6fce3c7043355a5dd7abb52d81f788dde78cd9fa
Hitesh Ambulkar <hitesh.ambulkar@gmail.com>
2020-08-12 18:50:51 -0400

    country data for checking GET call

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 $(document).ready(function()
 {
     init()
-
-
     function highest(myParamter){ 
         return myParamter.sort(function(a,b)
         { 
@@ -17,40 +15,37 @@ $(document).ready(function()
         var data='';
         $.get(url, function(data)
         {
-            // console.log(data);
-            // console.log(data.Countries[177]);
+            console.log(data);
+            console.log(data.Countries[177]);
             // console.log(data.Countries[177].Country);
             // console.log(data.Countries[177].TotalConfirmed);
             // console.log(data.Countries[177].TotalDeaths);
             // console.log(data.Countries[177].TotalRecovered);
             
-            
-
             var i;
             var output=new Array()
             for(i=0;i<186;i++)
             {
                 output[i]=data.Countries[i].TotalDeaths;
-                // console.log(output)
-                
-                // console.log(data.Countries[i].TotalDeaths)
             }
             console.log(highest(output));
-            console.log(output[0])
-            // output.sort()
-            // console.log(output[0])
+            console.log("Highest: "+output[0])
 
-            // var points = [400, 100, 1000, 5, 25, 160];
-            // console.log(points.sort(function(a, b){return b-a}));
-            // console.log(points[0])
-
-        //     data=
-        //     `
-        //     <td>${data.Global.TotalConfirmed}</td>
-        //     <td>${data.Global.TotalDeaths}</td>
-        //     <td>${data.Global.TotalRecovered}</td>
-        //     `
-        //     $("#data").html(data)
+            totaldata=
+            `
+            <td>${data.Global.TotalConfirmed}</td>
+            <td>${data.Global.TotalDeaths}</td>
+            <td>${data.Global.TotalRecovered}</td>
+            `
+            countrydata=
+            `
+            <td>${data.Countries[177].Country}</td>
+            <td>${data.Countries[177].TotalConfirmed}</td>
+            <td>${data.Countries[177].TotalDeaths}</td>
+            <td>${data.Countries[177].TotalRecovered}</td>
+            `
+            $("#totaldata").html(totaldata)
+            $("#countrydata").html(countrydata)
         })
     }
 })
